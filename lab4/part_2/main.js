561772f78b7a17c96ae43f1e5cc649eac0136c2a
LamarA1 <lamar.ahmadi@dcmail.ca>
2024-11-22 19:53:12 -0400

    Implemented Darken/Lighten button functionality.
    Added functionality to the button to toggle between darkening and lightening the overlay on the displayed image. The button's text content and class are updated accordingly, and the overlay's background color is adjusted to create the desired effect.

--- lab4/part_2/main.js
+++ lab4/part_2/main.js
@@ -32,3 +32,15 @@ thumbBar.addEventListener('click', (e) => {
 });
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    const currentClass = btn.getAttribute('class');
+    if (currentClass === 'dark') {
+      btn.setAttribute('class', 'light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class', 'dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+});
