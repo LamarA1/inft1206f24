380bae48ff8567e6cb688771e9738a163c23e636
LamarA1 <lamar.ahmadi@dcmail.ca>
2024-11-22 19:38:06 -0400

    Added custom name and unit conversion functionality.
    Added the ability to replace 'Bob' with a custom name provided by the user. Implemented unit conversion to change weight to stones and temperature to centigrade if the UK option is selected.

--- lab4/part_1/main.js
+++ lab4/part_1/main.js
@@ -25,6 +25,18 @@ function result() {
     newStory = newStory.replace(':insertx:', xItem);
     newStory = newStory.replace(':inserty:', yItem);
     newStory = newStory.replace(':insertz:', zItem);
+
+    if(customName.value !== '') {
+        const name = customName.value;
+        newStory = newStory.replace('Bob', name);
+    }
+      if(document.getElementById("uk").checked) {
+        const weight = Math.round(300 * 0.071429) + ' stone';
+        const temperature = Math.round((94 - 32) * 5 / 9) + ' centigrade';
+        newStory = newStory.replace('300 pounds', weight);
+        newStory = newStory.replace('94 fahrenheit', temperature);
+    }
+    
     story.textContent = newStory;
     story.style.visibility = 'visible';
 }
