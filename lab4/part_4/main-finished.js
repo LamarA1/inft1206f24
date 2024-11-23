efa8f7faa0b7362784a5386db72795ff2691a0fd
LamarA1 <lamar.ahmadi@dcmail.ca>
2024-11-22 20:41:55 -0400

    Add EvilCircle class with movement controls
    Defined the EvilCircle class extending from Shape and added movement controls for the evil circle.

--- lab4/part_4/main-finished.js
+++ lab4/part_4/main-finished.js
@@ -76,6 +76,63 @@ class Ball extends Shape {
   }
 }
 
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = 'white';
+    this.size = 10;
+    window.addEventListener('keydown', (e) => {
+      switch (e.key) {
+        case 'a':
+          this.x -= this.velX;
+          break;
+        case 'd':
+          this.x += this.velX;
+          break;
+        case 'w':
+          this.y -= this.velY;
+          break;
+        case 's':
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+  checkBounds() {
+    if (this.x + this.size >= width) {
+      this.x = width - this.size;
+    }
+    if (this.x - this.size <= 0) {
+      this.x = this.size;
+    }
+    if (this.y + this.size >= height) {
+      this.y = height - this.size;
+    }
+    if (this.y - this.size <= 0) {
+      this.y = this.size;
+    }
+  }
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+        }
+      }
+    }
+  }
+}
+
 const balls = [];
 
 while (balls.length < 25) {
@@ -92,6 +149,11 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+const evilCircle = new EvilCircle(
+  random(0 + 10, width - 10),
+  random(0 + 10, height - 10)
+);
+
 function loop() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
   ctx.fillRect(0, 0, width, height);
@@ -103,7 +165,10 @@ function loop() {
       ball.collisionDetect();
     }
   }
+  evilCircle.draw();
+  evilCircle.checkBounds();
+  evilCircle.collisionDetect();
 
-requestAnimationFrame(loop);
+  requestAnimationFrame(loop);
 }
 loop();
